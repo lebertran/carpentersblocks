5c23dad15098140c825b99beb12a98c81618b83d
lebertran <services@ktran.de>
2016-05-27 17:18:33 +0200

    model bake event, copyright

--- src/main/java/tk/eichler/carpentersblocks/Constants.java
+++ src/main/java/tk/eichler/carpentersblocks/Constants.java
@@ -1,13 +1,32 @@
+/*
+ * This file is part of Carpenter's Blocks.
+ *
+ * Carpenter's Blocks is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Carpenter's Blocks is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Carpenter's Blocks.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package tk.eichler.carpentersblocks;
 
 public class Constants {
 
-    /**
-     * These constants are replaced by gradle.
-     */
     public static final String MOD_ID = "carpentersblocks";
     public static final String MOD_NAME = "Carpenter's Blocks";
+
+    /**
+     * This constant is replaced by gradle.
+     */
     public static final String VERSION = "PROP:VERSION";
+
 
     /**
      * Proxy constants
